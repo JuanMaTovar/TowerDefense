076d53eef99bd8f1e438cec2dd6d50f90c93d547
JuanMaTovar <juan.tovar@utnay.edu.mx>
2024-02-12 15:29:32 -0700

    Animaciones
    Se agrega la animación pero no funciona.

--- Assets/_Scripts/Objetivo.cs
+++ Assets/_Scripts/Objetivo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Objetivo : MonoBehaviour
 {
+    public int vida = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,13 @@ public class Objetivo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (vida<= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void RecibirDano(int dano = 20)
+    {
+        vida -= dano;
     }
 }
