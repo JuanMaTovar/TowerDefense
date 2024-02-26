b6e3440caaf2fac0f29f2148b750160a34f7f79c
JuanMaTovar <juan.tovar@utnay.edu.mx>
2024-02-26 12:02:09 -0700

    modulo 21
    Entrega de actividad del módulo 21

---
+++ Assets/_Scripts/Zombie.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    public GameObject objetivo;
+    public int vida = 100;
+    public Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(objetivo.transform.position);
+        Anim = GetComponent<Animator>();
+        Anim.SetBool("IsMoving", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Objetivo")
+        {
+            Anim.SetBool("IsMoving", false);
+            Anim.SetTrigger("OnObjetiveReached");
+        }
+    }
+    public void Danar()
+    {
+        objetivo?.GetComponent<Objetivo>().RecibirDano(20);
+    }
+    public void RecibirDano(int dano = 10)
+    {
+        vida -= dano;
+    }
+}
